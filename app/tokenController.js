21e0e66b4548c8f753e2f9f40c74374add2a763d
Czekaj Mateusz <82756319+MateuszCzek332@users.noreply.github.com>
2025-05-31 14:50:21 +0200

    User Authorization

--- app/tokenController.js
+++ app/tokenController.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
+let badJWT = []
+
 module.exports = {
     verifyJWT: (req, res) => {
         const authHeader = req.headers['authorization'];
@@ -15,7 +17,38 @@ module.exports = {
         })
     },
     refreshJWT: (req, res) => {
-        //
+        const authHeader = req.headers['authorization'];
+        if(!authHeader) return res.sendStatus(401)
+        const token = authHeader.split(' ')[1]
+
+        if(badJWT.includes(token))
+            return res.sendStatus(403)
+        jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+            if(err) 
+                return res.sendStatus(403)
+
+            req.username = user.username
+        })
+
+        const accesToken = jwt.sign({username: req.username}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "30m"})
+        return res.json({
+            accesToken: accesToken,
+        })
+        
+    },
+    deleteJWT: (req, res) => {
+        const authHeader = req.headers['authorization'];
+        if(!authHeader) return res.sendStatus(401)
+        
+        const token = authHeader.split(' ')[1]
+        jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+            if(err) 
+                return res.sendStatus(403)
+
+            req.username = user.username
+        })
+
+        badJWT.push(token)
     },
 
 }
--- server.js
+++ server.js
@@ -3,19 +3,64 @@ require("dotenv").config()
 const express = require('express')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const tokenController = require("./app/tokenController");
 const app = express()
 const PORT = 3000;
 
 app.use(express.json());
 
+let users = []
+
 app.post("/register",  (req, res) => {
+    const {username, password} = req.body
+    if( !username || !password )
+        return res.sendStatus(400)
+        
+    if(users.find( (user) => username == user.username ))
+        return res.sendStatus(409)
 
 
+    bcrypt.hash(password, 8,  async (err, hash) => {
+        if (err)
+            return res.sendStatus(400)
+            
+
+        users.push({
+            username: username,
+            password: hash
+        })
+
+        return res.sendStatus(200)
+    });
 })
 
 app.post("/login", async (req, res) => {
+    const {username, password} = req.body
 
+    if( !username || !password )
+        return res.sendStatus(400)
 
+    const userInDB = users.find( (user) => username === user.username)
+    const compare = await bcrypt.compare(password, userInDB.password);
+    if(!compare)
+        return res.sendStatus(401)
+
+    const accesToken = jwt.sign({username: userInDB.username}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "30m"})
+    const refreshToken = jwt.sign({username: userInDB.username}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: "1d"})
+
+    return res.json({
+        accesToken: accesToken,
+        refreshToken: refreshToken
+    })
+
+})
+
+app.get("/refresh", async (req, res) => {
+    tokenController.refreshJWT(req, res)
+})
+
+app.post("/logout", async (req, res) => {
+    tokenController.deleteJWT(req, res)
 })
 
 app.listen(PORT, () => {
